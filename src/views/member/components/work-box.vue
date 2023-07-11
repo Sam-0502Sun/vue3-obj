670bffbde4fed0f81d8997792105af60216598e3
Sam <suntianxin52@sina.com>
2023-07-11 20:16:38 +0800

    Five

--- src/views/member/components/work-box.vue
+++ src/views/member/components/work-box.vue
@@ -13,7 +13,7 @@
       <el-table-column prop="city" label="City" width="120" />
       <el-table-column prop="address" label="Address" width="600" />
       <el-table-column prop="zip" label="Zip" width="120" />
-      <el-table-column fixed="right" label="Operations" width="120">
+      <el-table-column fixed="right" label="Options" width="120">
         <template #default="scope">
           <el-button link type="primary" size="small" @click="handleClick"
           >添加</el-button
